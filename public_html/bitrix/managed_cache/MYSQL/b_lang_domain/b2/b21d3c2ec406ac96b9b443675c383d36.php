cfcb7b74cabb455c231e88d8b96de68d5884f675
agalcev <forsagc@bitrix182.timeweb.ru>
2014-10-06 14:12:26 +0400

    reb

--- public_html/bitrix/managed_cache/MYSQL/b_lang_domain/b2/b21d3c2ec406ac96b9b443675c383d36.php
+++ public_html/bitrix/managed_cache/MYSQL/b_lang_domain/b2/b21d3c2ec406ac96b9b443675c383d36.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001412582761';
-$dateexpire = '001412586361';
+$datecreate = '001412590305';
+$dateexpire = '001412593905';
 $ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";a:0:{}}';
 return true;
 ?>
